5e2b86b4847dd6793005339041361a22067b77a2
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-24 22:37:06 +0530

    Chat GTP Response Working

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 import discord
 import os
+import openai
+import requests
 from dotenv import load_dotenv
 from discord.ext import commands
+load_dotenv()
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='$', intents=intents)
+
+
+
+
+# ChatGPT API endpoint
+#chatgpt_endpoint = "https://api.openai.com/v1/chat/completions"
+chatgpt_endpoint="https://api.openai.com/v1/chat/completions"
+
+# ChatGPT API access token
+chatgpt_api_key = os.environ.get('ChatGPT_API')
+print("chat gtp api key :" ,chatgpt_api_key )
 
 
 @bot.event
@@ -14,15 +28,40 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    print("Received message: ", message.content)
     if message.author == bot.user:
         return
-    if message.content.startswith('!hello'):
-        print("Sending response...")
-        await message.channel.send('hello')
+
+    # Check if message starts with command prefix
+    if message.content.startswith('$'):
+        # Get message content after the command prefix
+        msg = message.content[1:]
+
+        # Send message to ChatGPT API
+        headers = {"Authorization": f"Bearer {chatgpt_api_key}"}
+        payload = {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": msg}],
+            "temperature": 0.5,
+            "max_tokens": 60,
+            "n": 1,
+            "stop": "\n",
+
+        }
+        response = requests.post(chatgpt_endpoint, headers=headers, json=payload)
+        print('Responce Code : ', response.status_code)
+        print('Responce  : ', response.text)
+        # Get response from ChatGPT API
+        if response.status_code == 200:
+            data = response.json()
+            #response_text = data['choices'][0]['text']
+            response_text = response.json()['choices'][0]['message']['content']
+            await message.channel.send(response_text)
+        else:
+            await message.channel.send("Error: Unable to get response from ChatGPT API.")
+    else:
+        await bot.process_commands(message)
 
 
-load_dotenv()
 
 api_key = os.environ.get('Discord_Api')
 bot.run(api_key)
